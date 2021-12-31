7478aa456f2ffac22afd20199cf32b43743a513a
Cassiel <cassiel7@gmail.com>
2021-12-31 14:45:02 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include('./_globals_includes.php');
 <html lang="es" prefix="og: http://ogp.me/ns#" itemscope itemtype="http://schema.org/Article">
 <head>
 <?php
-	include('./modules/head.php');
+  include('./modules/head.php');
 ?>
 <title>Legal DNA - Home</title>
   </head>
@@ -26,13 +26,12 @@ include('./_globals_includes.php');
                 <h1>Soluciones LegalTech a tu medida </h1>
                 <p class="intro">Somos una Startup Legaltech Mexicana, brindamos una soluci&oacute;n tecnol&oacute;gica a tus necesidades legales</p>
                 <a class="btn" href="registro.php">CREAR CUENTA</a>
-                
               </div>
-            <div class="caption-piramide flex-item">
-              <video autoplay width="90%" height="90%" playsinline="" controlslist="nodownload" muted loop preload="auto">
-                <source src="./media/Comp_1.mp4"  type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
-              </video>
-            </div>
+              <div class="caption-piramide flex-item">
+                <video autoplay width="90%" height="90%" playsinline="" controlslist="nodownload" muted loop preload="auto">
+                  <source src="./media/Comp_1.mp4"  type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
+                </video>
+              </div>
           </div>    
         </div>
       </div>
@@ -48,7 +47,7 @@ include('./_globals_includes.php');
           </div>
         </div>
       </div>
-  		-->
+      -->
 
 
       <div class="mdSlg">
@@ -58,20 +57,20 @@ include('./_globals_includes.php');
         </div>
       </div>
 
+
       <div class="mdDts bdDwn">
         <div class="lmts"> 
           <div class="thumb">
-            <video class="versus" width="75%" height="75%" controls>
+            <video class="versus" width="465" controls>
                 <source src="./media/versus.mp4"  type="video/mp4">
                 <source src="./media/versus.webm"  type="video/webm">
                 <source src="./media/versus.ogg"  type="video/ogg">
-              </video>
+            </video>
           </div>
           <div class="nfrmtv">
             <h5>¿Qu&eacute; es LegalTech?</h5>
-            <p>El uso de la tecnolog&iacute;a aplicada a la prestaci&oacute;n de servicios legales que pueden ser estandarizables con la finalidad de aumentar la rentabilidad, reduciendo tiempos y costos.
-            </p>
-            <h4>¿Cómo migrar a un entorno digital ?</h4>
+            <p>El uso de la tecnolog&iacute;a aplicada a la prestaci&oacute;n de servicios legales que pueden ser estandarizables con la finalidad de aumentar la rentabilidad, reduciendo tiempos y costos.</p>
+            <h4>¿Cómo migrar a un entorno digital?</h4>
             <p>Nuestra Plataforma ofrece un Sistema de Firma Electr&oacute;nica Avanzada con la cual obtendr&aacute;s mayor agilidad y eficacia probatoria ya que las partes podr&aacute;n firmar desde cualquier lugar en el que se encuentren y tendr&aacute;n los mismos efectos legales con mayor certeza jur&iacute;dica sobre dichos documentos.
             </p>
             <button class="btn">CONOCER M&Aacute;S</button>
@@ -81,7 +80,7 @@ include('./_globals_includes.php');
 
       <div class="mdSlg">
         <div class="lmts"> 
-          <h2>Aspectos legales</h2>
+          <h2 class="tit-aspectos"><a href="<?php echo URLS_FOR_MENUS.'aspectos.php'; ?>">Aspectos legales</a></h2>
         </div>
       </div>
 
@@ -99,7 +98,6 @@ include('./_globals_includes.php');
                 <p><span>Legal DNA</span>, es una herramienta muy &uacute;til, sin duda ayud&oacute; a mejorar nuestros procesos ya que para nosotros es importante mantener la seguridad de la informaci&oacute;n.</p>
                 <div class="prfl espacio">
                   <p class="directivo">3er ESPACIO</p>
-                  <!-- <label>CEO, Company</label> -->
                 </div>
               </div>
             </li>
@@ -108,17 +106,10 @@ include('./_globals_includes.php');
                 <p><span>Legal DNA</span> es una soluci&oacute;n, pr&aacute;ctica, innovadora y funcional que ayuda simplificar tu vida legal tomando como base las necesidades legales de hoy en d&iacute;a y cumpliendo con la normatividad actual; Optimiza tus recursos y te da accesibilidad en cualquier parte del mundo! Herramientas como estas no solo son un plus sino una necesidad a las actuales demandas del mercado y diferentes autoridades.</p>
                 <div class="prfl nc">
                   <p class="directivo">C&N CONSULTING</p>
-                  <!-- <label>CEO, Company</label> -->
                 </div>
               </div>
             </li>
           </ul>
-          <!--
-          <div class="sldCtrl">
-            <button class="btn rwrd">&nbsp;</button>
-            <button class="btn fwrd">&nbsp;</button>
-          </div>
-        -->
         </div>
       </div>
 
